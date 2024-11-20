2c7687c21df088ee383086fbdd62393c47d83829
Roman Lysyk <r.lysyk@synchronic.info>
2024-11-20 14:21:27 +0200

    fix event

--- src/containers/Layout/index.tsx
+++ src/containers/Layout/index.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useMemo, ReactNode } from 'react';
 import { Outlet, useNavigate } from 'react-router-dom';
+import { Helmet } from 'react-helmet-async';
 
 import { baseDataAPI } from '../../services/baseDataService';
 import { getFromStorage } from '../../lib';
@@ -29,9 +30,25 @@ export const Layout = ({ children }: { children?: ReactNode }) => {
 		if(cartStorage.length !== 0) dispatch(addCartFromStorage(cartStorage));
 	}, [bookmarksStorage, cartStorage, comparisonStorage, dispatch]);
 
+	useEffect(() => {
+		const script = document.createElement('noscript');
+		script.innerHTML = '<iframe src="https://www.googletagmanager.com/ns.html?id=G-MPBS7BE0VP"\n' +
+			'height="0" width="0" style="display:none;visibility:hidden"></iframe>';
+		document.body.appendChild(script);
+	}, []);
+
 	return <>
-		<Header />
-		{ children || <Outlet /> }
-		<Footer />
+		<Helmet>
+			<script>
+				{ `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+				new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+				j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+				'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+				})(window,document,'script','dataLayer','G-MPBS7BE0VP');` }
+			</script>
+		</Helmet>
+		<Header/>
+		{children || <Outlet/>}
+		<Footer/>
 	</>
 };
