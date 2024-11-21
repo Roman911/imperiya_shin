d931cf18a7e50ca4ff0609f0223381f2bd6881ec
Roman Lysyk <r.lysyk@synchronic.info>
2024-11-21 22:04:58 +0200

    fix event

--- src/containers/Layout/index.tsx
+++ src/containers/Layout/index.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useMemo, ReactNode } from 'react';
 import { Outlet, useNavigate } from 'react-router-dom';
-import { Helmet } from 'react-helmet-async';
 
 import { baseDataAPI } from '../../services/baseDataService';
 import { getFromStorage } from '../../lib';
@@ -31,22 +30,27 @@ export const Layout = ({ children }: { children?: ReactNode }) => {
 	}, [bookmarksStorage, cartStorage, comparisonStorage, dispatch]);
 
 	useEffect(() => {
-		const script = document.createElement('noscript');
-		script.innerHTML = '<iframe src="https://www.googletagmanager.com/ns.html?id=G-MPBS7BE0VP"\n' +
-			'height="0" width="0" style="display:none;visibility:hidden"></iframe>';
-		document.body.appendChild(script);
+		const script1 = document.createElement('script');
+		script1.src = "https://www.googletagmanager.com/gtag/js?id=G-MPBS7BE0VP";
+		script1.async = true;
+		document.body.appendChild(script1);
+
+		const script2 = document.createElement('script');
+		script2.innerHTML = `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-MPBS7BE0VP');
+    `;
+		document.body.appendChild(script2);
+
+		return () => {
+			document.body.removeChild(script1);
+			document.body.removeChild(script2);
+		};
 	}, []);
 
 	return <>
-		<Helmet>
-			<script>
-				{ `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-				new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-				j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-				'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-				})(window,document,'script','dataLayer','G-MPBS7BE0VP');` }
-			</script>
-		</Helmet>
 		<Header/>
 		{children || <Outlet/>}
 		<Footer/>
