9e562a998a9e5b32dd61fa869eaef77ed64ebd26
romanlysyk <roma-lysyk@ukr.net>
2024-12-04 22:31:02 +0200

    fix test redirect

--- src/containers/Order/index.tsx
+++ src/containers/Order/index.tsx
@@ -1,21 +1,23 @@
 import { useEffect, useMemo, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
 import { Helmet } from 'react-helmet-async';
 import { FormProvider, useForm, SubmitHandler } from 'react-hook-form';
 import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
-import { FetchBaseQueryError } from '@reduxjs/toolkit/query';
-import { SerializedError } from '@reduxjs/toolkit';
+// import { FetchBaseQueryError } from '@reduxjs/toolkit/query';
+// import { SerializedError } from '@reduxjs/toolkit';
 
 import { config } from '../../config';
 import { baseDataAPI } from '../../services/baseDataService';
-import { useAppDispatch, useAppGetProducts, useAppSelector, useAppTranslation } from '../../hooks';
-import { reset } from '../../store/reducers/cartSlice';
+// import { useAppDispatch, useAppGetProducts, useAppSelector, useAppTranslation } from '../../hooks';
+import { useAppGetProducts, useAppSelector, useAppTranslation } from '../../hooks';
+// import { reset } from '../../store/reducers/cartSlice';
 import { OrderComponent } from '../../components/Order';
 import { Title } from '../../components/Lib';
 import { LayoutWrapper } from '../../components/Layout';
 import { Breadcrumbs } from '../../components/Breadcrumbs';
-import { trackBeginCheckout, trackPurchase } from '../../event';
+// import { trackBeginCheckout, trackPurchase } from '../../event';
+import { trackBeginCheckout } from '../../event';
 
 const scrollToTop = () => {
 	window.scrollTo({
@@ -55,17 +57,18 @@ const defaultValues = {
 }
 
 export const Order = () => {
-	const navigate = useNavigate();
-	const dispatch = useAppDispatch();
+	// const navigate = useNavigate();
+	// const dispatch = useAppDispatch();
 	const [loadingBtn, setLoadingBtn] = useState(false);
 	const [shippingMethod, setShippingMethod] = useState<number | string | undefined>(1);
-	const [paymentMethod, setPaymentMethod] = useState<number | string | undefined>(1);
+	// const [paymentMethod, setPaymentMethod] = useState<number | string | undefined>(1);
 	const { cartItems } = useAppSelector(state => state.cartReducer);
-	const { city, wirehouse } = useAppSelector(state => state.orderReducer);
+	// const { city, wirehouse } = useAppSelector(state => state.orderReducer);
+	const { city } = useAppSelector(state => state.orderReducer);
 	const t = useAppTranslation();
 	const { tires, cargo, disks, battery, isLoading} = useAppGetProducts(cartItems, 'reducerCart', true);
 	const { data: dataOrdersParam } = baseDataAPI.useFetchOrdersParamQuery('');
-	const [ createOrder ] = baseDataAPI.useCreateOrderMutation();
+	// const [ createOrder ] = baseDataAPI.useCreateOrderMutation();
 
 	const newData = useMemo(() => ({
 		result: true,
@@ -83,24 +86,24 @@ export const Order = () => {
 		trackBeginCheckout(newData, cartItems);
 	}, [cartItems, newData]);
 
-	const products = newData?.data.products?.map((item) => {
-		return {
-			product_id: item.product_id,
-			offer_id: item.best_offer.id,
-			price: Number(item.best_offer.price),
-			quantity: cartItems.find(i => i.id === item.best_offer.id)?.quantity,
-		}
-	});
+	// const products = newData?.data.products?.map((item) => {
+	// 	return {
+	// 		product_id: item.product_id,
+	// 		offer_id: item.best_offer.id,
+	// 		price: Number(item.best_offer.price),
+	// 		quantity: cartItems.find(i => i.id === item.best_offer.id)?.quantity,
+	// 	}
+	// });
+	//
+	// const items = newData?.data.products.map(item => {
+	// 	const id = item.best_offer.id;
+	// 	const price = item.best_offer.price;
+	// 	const quantity = cartItems.find(i => i.id === id)?.quantity;
+	//
+	// 	return { id, price, quantity }
+	// });
 
-	const items = newData?.data.products.map(item => {
-		const id = item.best_offer.id;
-		const price = item.best_offer.price;
-		const quantity = cartItems.find(i => i.id === id)?.quantity;
-
-		return { id, price, quantity }
-	});
-
-	const total = items?.reduce((sum, item) => sum + (item.quantity ?? 0) * parseFloat(item.price), 0);
+	// const total = items?.reduce((sum, item) => sum + (item.quantity ?? 0) * parseFloat(item.price), 0);
 
 	const methods = useForm<FormProps>({
 		mode: 'all',
@@ -121,8 +124,9 @@ export const Order = () => {
 		},
 	];
 
-	const onSubmit: SubmitHandler<FormProps> = async (data) => {
-		const { firstname, lastname, surname, email, telephone, comment, address } = data;
+	const onSubmit: SubmitHandler<FormProps> = async () => {
+	// const onSubmit: SubmitHandler<FormProps> = async (data) => {
+		//const { firstname, lastname, surname, email, telephone, comment, address } = data;
 		setLoadingBtn(true);
 		window.open('https://rozetka.com.ua/ua/', "_blank");
 		// await createOrder({
@@ -166,7 +170,7 @@ export const Order = () => {
 		if(name === 'shipping_method'){
 			setShippingMethod(value);
 		} else if(name === 'payment_method'){
-			setPaymentMethod(value);
+			// setPaymentMethod(value);
 		}
 	}
 
